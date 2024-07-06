1b652fab284d6b8b26d34541fda3899e81f54813
Liliana Tuta <lilianatutza@gmail.com>
2024-07-06 10:25:09 +0100

    CSS and prompts instructions

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "bb604a31635tbbcod35f93428afe08ae";
   let prompt = `User instructions: Generate a English Poem about ${instructionsIput.value}`;
   let context =
-    "you are a sarcastic Poem expert and love to write poems. Your mission is to generate a 4 line poem in basic HTML and seaparate each line with a <br />. Make sure to follow the user instructions. Sign the poem at the bottom at the poem with SheCodes AI, inside a <strong> element. Do not include a line before the poem";
+    "you are a romantic Poem expert and love to write poems. Your mission is to generate a 4 line poem in basic HTML and seaparate each line with a <br />. Make sure to follow the user instructions. Sign the poem at the bottom at the poem with SheCodes AI, inside a <strong> element. Do not include a line before the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem");
